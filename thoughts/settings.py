f6a203df6338263a2f834666816e5a0eb63c165c
VikasAdhikari07 <91743784+VikasAdhikari07@users.noreply.github.com>
2024-10-10 20:33:12 +0530

    Update settings.py

--- thoughts/settings.py
+++ thoughts/settings.py
@@ -125,5 +125,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_URL = '/accounts/login'
 
-LOGIN_REDIRECT_URL ='/tweet'
-LOGOUT_REDIRECT_URL = '/tweet'
+LOGIN_REDIRECT_URL ='/'
+LOGOUT_REDIRECT_URL = '/'
